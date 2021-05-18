dd934684ba12113cfb23eed83c35bdb34fda961c
Caique Araujo <caique@studiopiggly.com.br>
2021-05-18 14:47:10 -0300

    [wip] Decimalmethods abs, ceil, cmp

--- src/Decimal.php
+++ src/Decimal.php
@@ -11,7 +11,7 @@ class Decimal
 	 * @var array<int>|null
 	 * @since 1.0.0
 	 */
-	protected $_digit;
+	protected $_digits;
 
 	/**
 	 * An integer between -9e15
@@ -37,16 +37,113 @@ class Decimal
 	public function __construct ( $number )
 	{}
 
+	/**
+	 * Returns a new Decimal whose value is the absolute value.
+	 * i.e. the magnitude, of the value of this Decimal.
+	 * 
+	 * The return value is not affected by the value of the 
+	 * precision setting.
+	 *
+	 * @since 1.0.0
+	 * @return Decimal
+	 */
+	public function absoluteValue () : Decimal
+	{ 
+		$decimal = new Decimal($this);
+		
+		if ( $decimal->_sign < 0 )
+		{ $decimal->_sign = 1; }
+
+		return $this->finalize($decimal);
+	}
+
+	/**
+	 * Alias to absoluteValue() method.
+	 *
+	 * @see absoluteValue()
+	 * @since 1.0.0
+	 * @return Decimal
+	 */
 	public function abs () : Decimal
-	{}
+	{ return $this->absoluteValue(); }
 
+	/**
+	 * Return a new Decimal whose value is the value 
+	 * of this Decimal rounded to a whole number in 
+	 * the direction of positive Infinity.
+	 *
+	 * @since 1.0.0
+	 * @return Decimal
+	 */
 	public function ceil () : Decimal
-	{}
+	{ return $this->finalize(new Decimal($this), $this->_exponent + 1, DecimalConfig::ROUND_CEIL); }
 
-	public function cmp ( $number ) : Decimal
-	{}
+	/**
+	 * Return
+	 *   1    if the value of this Decimal is greater than the value of `y`,
+	 *  -1    if the value of this Decimal is less than the value of `y`,
+	 *   0    if they have the same value,
+	 *   NAN  if the value of either Decimal is NAN.
+	 *
+	 * @param Decimal|float|int|string $y
+	 * @return float
+	 */
+	public function comparedTo ( $y ) : float
+	{
+		$x = $this;
+		$xd = $x->_digits;
+		$xs = $x->_sign;
 
-	public function cbrt () : Decimal
+		$y = new Decimal($y);
+		$yd = $y->_digits;
+		$ys = $y->_sign;
+
+		// Either NaN or ±Infinity?
+		if ( is_null($xd) || is_null($yd) )
+		{ return is_null($xs) || is_null($ys) ? \NAN : ($xs !== $ys ? 0 : ($xs === $ys ? 0 : (!$xd ^ $xs < 0 ? 1 : -1))); }
+
+		// Either zero?
+		if ( !isset($xd[0]) || !isset($yd[0]) )
+		{ return isset($xd[0]) ? $xs : (isset($yd[0]) ? -$ys : 0); }
+
+		// Signs differ?
+		if ($xs !== $ys) 
+		{ return $xs; }
+
+		// Compare exponents
+		if ( $x->_exponent !== $y->_exponent )
+		{ return $x->_exponent > $y->_exponent ^ $xs < 0 ? 1 : -1; }
+
+		$xdC = count($xd);
+		$ydC = count($yd);
+
+    	// Compare digit by digit
+		for (
+			$i = 0,
+			$j = $xdC < $ydC ? $xdC : $ydC;
+			$i < $j;
+			++$i
+		)
+		{
+			if ( $xd[$i] !== $yd[$i] ) 
+			{ return $xd[$i] > $yd[$i] ^ $xs < 0 ? 1 : -1; }
+		}
+
+		// Compare lengths
+		return $xdC === $ydC ? 0 : ( $xdC > $ydC ^ $xs < 0 ? 1 : -1 ); 
+	}
+
+	/**
+	 * Alias to comparedTo() method.
+	 *
+	 * @see comparedTo()
+	 * @since 1.0.0
+	 * @return float
+	 */
+	public function cmp ( $y )
+	{ return $this->comparedTo($y); }
+
+	public function cbrt () : float
 	{}
 
 	public function dp () : Decimal
@@ -209,5 +306,13 @@ class Decimal
 	{}
 
 	public function valueOf () : string
+	{}
+
+	protected function finalize ( 
+		$number, 
+		$significantDigits = null, 
+		$rouding = null, 
+		$isTruncated = false
+	) : Decimal
 	{}
 }
